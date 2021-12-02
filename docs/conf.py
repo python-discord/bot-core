d851777945cb5ba638b0c3bfd07bd6418132c840
Hassan Abouelela <hassan@hassanamr.com>
2021-12-03 02:19:32 +0400

    Update Doc Entry Color

--- docs/conf.py
+++ docs/conf.py
@@ -63,6 +63,12 @@ exclude_patterns = []
 #
 html_theme = "furo"
 
+html_theme_options = {
+    "light_css_variables": {
+        "color-api-name": "var(--color-link)",
+    },
+}
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
