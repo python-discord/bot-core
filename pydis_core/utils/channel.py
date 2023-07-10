6ff9b4992e9f7991c727c65450650dd332a33511
shtlrs <amrbellalouna@gmail.com>
2023-07-10 21:00:38 +0100

    unindent overly indented signature

--- pydis_core/utils/channel.py
+++ pydis_core/utils/channel.py
@@ -23,8 +23,8 @@ def is_in_category(channel: discord.TextChannel, category_id: int) -> bool:
 
 
 async def get_or_fetch_channel(
-        bot: Bot,
-        channel_id: int
+    bot: Bot,
+    channel_id: int,
 ) -> discord.abc.GuildChannel | discord.abc.PrivateChannel | discord.Thread:
     """
     Attempt to get or fetch the given ``channel_id`` from the bots cache, and return it.
