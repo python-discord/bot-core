b892c86eace80bfb05d2c690477496d651b4d4b1
Chris Lovering <chris.lovering.95@gmail.com>
2024-09-11 11:34:37 +0100

    Remove an unneeded noqa

--- tests/pydis_core/utils/test_cooldown.py
+++ tests/pydis_core/utils/test_cooldown.py
@@ -1,7 +1,7 @@
 import unittest
 from unittest.mock import patch
 
-from pydis_core.utils.cooldown import _CommandCooldownManager, _create_argument_tuple  # noqa: PLC2701
+from pydis_core.utils.cooldown import _CommandCooldownManager, _create_argument_tuple
 
 
 class CommandCooldownManagerTests(unittest.IsolatedAsyncioTestCase):
